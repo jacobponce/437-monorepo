45c0392e89b471912643212d841db726c6b0175d
jacobponce <ponce.jacob.t@gmail.com>
2025-11-09 22:09:14 -0800

    Lab10, setting up MongoDB and fetching data from mongoDB into the home page.

---
+++ packages/server/src/index.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response } from "express";
+import { connect } from "./services/mongo";
+import FeatureCards from "./services/feature-card-svc";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const staticDir = process.env.STATIC || "public";
+
+connect("golf_features");
+
+app.use(express.static(staticDir));
+
+app.get("/hello", (req: Request, res: Response) => {
+  res.send("Hello, World");
+});
+
+app.get("/features", (req: Request, res: Response) => {
+  FeatureCards.index().then((data) => {
+    res
+      .set("Content-Type", "application/json")
+      .send(JSON.stringify(data));
+  });
+});
+
+app.get("/features/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  FeatureCards.get(id).then((data) => {
+    if (data) {
+      res
+        .set("Content-Type", "application/json")
+        .send(JSON.stringify(data));
+    } else {
+      res.status(404).send();
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
